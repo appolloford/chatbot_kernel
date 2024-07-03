1cb06b4394974290eff287d5069d81d4325e07c1
Chia-Jung Hsu <appolloford@gmail.com>
2024-07-03 15:40:26 +0200

    Do simple formatting

--- chatbot_kernel/__main__.py
+++ chatbot_kernel/__main__.py
@@ -3,8 +3,7 @@ from ipykernel.kernelapp import IPKernelApp
 from . import ChatbotKernel
 from .kernelspec import main as install_main
 
-if len(sys.argv) > 1 and sys.argv[1] == 'install':
+if len(sys.argv) > 1 and sys.argv[1] == "install":
     install_main(sys.argv[2:])
 else:
     IPKernelApp.launch_instance(kernel_class=ChatbotKernel)
-
--- chatbot_kernel/kernelspec.py
+++ chatbot_kernel/kernelspec.py
@@ -14,13 +14,14 @@ kernel_json = {
     "language": "text",
 }
 
+
 def install_chatbot_kernel_spec(user=True, prefix=None):
     with TemporaryDirectory() as td:
         os.chmod(td, 0o755)  # Starts off as 700, not user readable
-        with open(os.path.join(td, 'kernel.json'), 'w') as f:
+        with open(os.path.join(td, "kernel.json"), "w") as f:
             json.dump(kernel_json, f, sort_keys=True)
 
-        print('Installing Chatbot Kernel spec')
+        print("Installing Chatbot Kernel spec")
         # Requires logo files in kernel root directory
         cur_path = os.path.dirname(os.path.realpath(__file__))
         for logo in ["logo-32x32.png", "logo-64x64.png"]:
@@ -29,7 +30,8 @@ def install_chatbot_kernel_spec(user=True, prefix=None):
             except FileNotFoundError:
                 print("Custom logo files not found. Default logos will be used.")
 
-        KernelSpecManager().install_kernel_spec(td, 'chatbot', user=user, prefix=prefix)
+        KernelSpecManager().install_kernel_spec(td, "chatbot", user=user, prefix=prefix)
+
 
 def _is_root():
     try:
@@ -37,27 +39,26 @@ def _is_root():
     except AttributeError:
         return False  # assume not an admin on non-Unix platforms
 
+
 def main(argv=None):
-    parser = argparse.ArgumentParser(
-        description='Install KernelSpec for Chatbot Kernel'
-    )
+    parser = argparse.ArgumentParser(description="Install KernelSpec for Chatbot Kernel")
     prefix_locations = parser.add_mutually_exclusive_group()
 
     prefix_locations.add_argument(
-        '--user',
-        help='Install KernelSpec in user home directory',
-        action='store_true'
+        "--user",
+        help="Install KernelSpec in user home directory",
+        action="store_true",
     )
     prefix_locations.add_argument(
-        '--sys-prefix',
-        help='Install KernelSpec in sys.prefix. Useful in conda / virtualenv',
-        action='store_true',
-        dest='sys_prefix'
+        "--sys-prefix",
+        help="Install KernelSpec in sys.prefix. Useful in conda / virtualenv",
+        action="store_true",
+        dest="sys_prefix",
     )
     prefix_locations.add_argument(
-        '--prefix',
-        help='Install KernelSpec in this prefix',
-        default=None
+        "--prefix",
+        help="Install KernelSpec in this prefix",
+        default=None,
     )
 
     args = parser.parse_args(argv)
@@ -73,5 +74,6 @@ def main(argv=None):
 
     install_chatbot_kernel_spec(user=user, prefix=prefix)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
